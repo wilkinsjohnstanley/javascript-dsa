39ca7dfb9afe9de3ff11925bcdc57af313760101
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-08-27 12:23:01 -0500

    don't forget the return statement

--- 7-fibonacci-sequence.js
+++ 7-fibonacci-sequence.js
@@ -1,8 +1,8 @@
 function fibonacci(n){
-    const fib = [0, 1];
+    const fib = [0, 1]
     for ( let i = 2; i < n; i++){
 //the previous two numbers will be the index minus one and minus two respectively.         
-        fib[i]=fib[i-1]+[i-2];
+        fib[i]=fib[i-1]+[i-2]
     }
 
     
