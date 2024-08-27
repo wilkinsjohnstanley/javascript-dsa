5e3770586d66bb31815f5fd14da2b8b4ced75cf8
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-08-27 12:27:06 -0500

    commit

--- 7-fibonacci-sequence.js
+++ 7-fibonacci-sequence.js
@@ -2,10 +2,15 @@ function fibonacci(n){
     const fib = [0, 1]
     for ( let i = 2; i < n; i++){
 //the previous two numbers will be the index minus one and minus two respectively.         
-        fib[i]=fib[i-1]+[i-2]
+        fib[i]=fib[i-1]+fib[i-2]
     }
     return fib
     
 }
 
-console.log(fibonacci(2))
+console.log(fibonacci(7))
+
+//try tracing the execution with a pen and paper
+
+
+//big O is O(n) Linear time complexity. 
